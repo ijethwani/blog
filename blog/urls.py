8e9846d25c2563f9db307e56dd4520bcc6c67ff9
ishwar <ishwarjethwani12@gmail.com>
2019-12-10 11:37:57 +0530

    upload

---
+++ blog/urls.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+from posts.views import (
+    index,
+    search,
+    post_list,
+    post_detail,
+    post_create,
+    post_update,
+    post_delete,
+    IndexView,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+from marketing.views import email_list_signup
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='home'),
+    path('about/', views.About, name='about'),
+    path('blog/', PostListView.as_view(), name='post-list'),
+    path('search/', search, name='search'),
+    path('email-signup/', email_list_signup, name='email-list-signup'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
+    path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('tinymce/', include('tinymce.urls')),
+    path('accounts/', include('allauth.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
